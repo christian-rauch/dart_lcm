112accc6851833fcf77cd4f1464f9635debb2749
Christian Rauch <Christian.Rauch@ed.ac.uk>
2016-06-08 10:49:49 +0100

    fix constructing of new map entries when accessing nonexistent keys

--- src/dart_lcm_joints.cpp
+++ src/dart_lcm_joints.cpp
@@ -52,20 +52,24 @@ int LCM_JointsProvider::next(const int time_ms) {
 }
 
 void LCM_JointsProvider::handle_msg_joints(const lcm::ReceiveBuffer* rbuf, const std::string& channel, const bot_core::robot_state_t* msg) {
-    // set default values to NaN, so we can check if joints are present
-    _joint_values = std::vector<float>(_joint_names.size(), std::numeric_limits<float>::quiet_NaN());
+    // allocate memory for amount of expected joints
+    _joint_values.resize(_joint_names.size());
 
-    // hashtable for faster search of joitn names and values
+    // hashtable for faster search of joint names and values
     std::map<std::string, float> joints;
+    std::transform(msg->joint_name.begin(), msg->joint_name.end(),  // key range
+                   msg->joint_position.begin(),                     // value range
+                   std::inserter(joints, joints.end()),             // target map
+                   std::make_pair<std::string const&, float const&>); // key-value pair
 
-    std::transform(msg->joint_name.begin(), msg->joint_name.end(), msg->joint_position.begin(),
-                   std::inserter(joints, joints.end()),
-                   std::make_pair<std::string const&, float const&>);
-
-    // joint values need to be ordered as they appear in '_joint_names'
+    // order '_joint_values' as they appear in '_joint_names'
+    // We need to check for the existence of a key before using the [] operator,
+    // otherwise a new entry with this key and default constructed element will
+    // be generated. Use NaN value if key is not present to determine omitted
+    // joints later using std::isnan().
     for(unsigned int i=0; i<_joint_names.size(); i++) {
-        _joint_values[i] = joints[_joint_names[i]];
+        _joint_values[i] = (joints.count(_joint_names[i])!=0) ? joints[_joint_names[i]] : NAN;
     }
 }
 
-}
+} // namespace dart
